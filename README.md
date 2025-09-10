d0eed2547efab18957cea9b76077315b41a7ebd1
IceSentry <c.giguere42@gmail.com>
2025-09-10 14:29:12 -0400

    basic triangle rendering with aabb

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Glaciers
 
 A simple cpu rasterizer made with bevy
+
+## Resources used
+
+- https://jtsorlinis.github.io/rendering-tutorial/
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::time::Instant;
+
 use bevy::{
     asset::RenderAssetUsages,
-    color::palettes::css::{BLUE, GREEN, RED, WHITE},
+    color::palettes::css::{BLACK, BLUE, GREEN, RED, WHITE},
     core_pipeline::core_3d::graph::{Core3d, Node3d},
     ecs::query::QueryItem,
     image::TextureFormatPixelInfo,
@@ -77,7 +79,7 @@ fn setup(
     window: Query<&Window, With<PrimaryWindow>>,
 ) {
     let res = &window.single().unwrap().resolution;
-    let scale = 0.1;
+    let scale = 0.25;
     let image = Image::new_fill(
         Extent3d {
             width: (res.width() * scale) as u32,
@@ -138,6 +140,7 @@ fn update(
     mut resize_events: EventReader<WindowResized>,
     triangles: Query<(Entity, &Triangle)>,
     keyboard: Res<ButtonInput<KeyCode>>,
+    mut window: Query<&mut Window, With<PrimaryWindow>>,
 ) -> Result<()> {
     let Ok(glaciers_context) = glaciers_context.single_mut() else {
         return Ok(());
@@ -146,38 +149,42 @@ fn update(
         return Ok(());
     };
 
+    let start = Instant::now();
+
     if keyboard.just_pressed(KeyCode::KeyR) {
         for (e, _) in triangles {
             commands.entity(e).despawn();
         }
-        let point_a = UVec2::new(
-            fastrand::u32(0..image.size().x),
-            fastrand::u32(0..image.size().y),
-        );
-
-        let point_b = UVec2::new(
-            fastrand::u32(0..image.size().x),
-            fastrand::u32(0..image.size().y),
-        );
-
-        let point_c = UVec2::new(
-            fastrand::u32(0..image.size().x),
-            fastrand::u32(0..image.size().y),
-        );
-
         commands.spawn(Triangle {
-            points: [point_a, point_b, point_c],
+            points: [
+                UVec2::new(
+                    fastrand::u32(0..image.size().x),
+                    fastrand::u32(0..image.size().y),
+                ),
+                UVec2::new(
+                    fastrand::u32(0..image.size().x),
+                    fastrand::u32(0..image.size().y),
+                ),
+                UVec2::new(
+                    fastrand::u32(0..image.size().x),
+                    fastrand::u32(0..image.size().y),
+                ),
+            ],
         });
     }
 
     // Resize when needed
     for ev in resize_events.read() {
-        image.resize(Extent3d {
-            width: (ev.width * glaciers_context.scale) as u32,
-            height: (ev.height * glaciers_context.scale) as u32,
-            depth_or_array_layers: 1,
-        });
-        println!("Image size: {}", image.size());
+        if image.size_f32().x != ev.width * glaciers_context.scale
+            || image.size_f32().y != ev.height * glaciers_context.scale
+        {
+            image.resize(Extent3d {
+                width: (ev.width * glaciers_context.scale) as u32,
+                height: (ev.height * glaciers_context.scale) as u32,
+                depth_or_array_layers: 1,
+            });
+            println!("Image size: {} ", image.size());
+        }
     }
 
     // Clear the image
@@ -187,30 +194,67 @@ fn update(
         }
     }
 
-    // TODO use a separate buffer for the image?
-
     let half_width = image.size().x / 2;
     let half_height = image.size().y / 2;
-    let center = UVec2::new(half_width, half_height);
+    let _center = UVec2::new(half_width, half_height);
 
     for (_, triangle) in triangles {
-        // let aabb = Aabb2d::from_point_cloud(Vec2::ZERO, &triangle.points.map(|p| p.as_vec2()));
-        //
-        // let (min, max) = triangle.points.iter().fold(
-        //     (triangle.points[0], triangle.points[0]),
-        //     |(prev_min, prev_max), point| (point.min(prev_min), point.max(prev_max)),
-        // );
-
-        draw_line(image, triangle.points[0], triangle.points[1], WHITE.into());
-        draw_line(image, triangle.points[1], triangle.points[2], WHITE.into());
-        draw_line(image, triangle.points[2], triangle.points[0], WHITE.into());
-
-        draw_point(image, triangle.points[0], RED.into());
-        draw_point(image, triangle.points[1], GREEN.into());
-        draw_point(image, triangle.points[2], BLUE.into());
+        draw_triangle(image, triangle.points);
     }
 
+    window.single_mut().unwrap().title = format!(
+        "Glaciers - {}x{} {:.3}ms",
+        image.size().x,
+        image.size().y,
+        start.elapsed().as_secs_f32() / 1000.0
+    );
+
     Ok(())
+}
+
+fn edge_function(a: IVec2, b: IVec2, c: IVec2) -> i32 {
+    (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
+}
+
+fn draw_triangle(image: &mut Image, points: [UVec2; 3]) {
+    // Compute AABB of the triangle
+    let (min, max) = points
+        .iter()
+        .fold((points[0], points[0]), |(prev_min, prev_max), point| {
+            (point.min(prev_min), point.max(prev_max))
+        });
+
+    // Only check the pixels inside the AABB
+    for x in min.x..=max.x {
+        for y in min.y..=max.y {
+            let a = points[0].as_ivec2();
+            let b = points[1].as_ivec2();
+            let c = points[2].as_ivec2();
+
+            let p = UVec2::new(x, y).as_ivec2();
+            let abp = edge_function(a, b, p);
+            let bcp = edge_function(b, c, p);
+            let cap = edge_function(c, a, p);
+
+            // This is only needed because winding order is random right now.
+            // Normally you only need to check if it's > 0.0
+            if (abp >= 0 && bcp >= 0 && cap >= 0) || (abp <= 0 && bcp <= 0 && cap <= 0) {
+                draw_point(image, p.as_uvec2(), WHITE.into());
+            } else {
+                draw_point(image, p.as_uvec2(), RED.into());
+            }
+        }
+    }
+
+    // Draw the outline useful for wireframe mode
+    draw_line(image, points[0], points[1], BLACK.into());
+    draw_line(image, points[1], points[2], BLACK.into());
+    draw_line(image, points[2], points[0], BLACK.into());
+
+    // Draw each corners
+    draw_point(image, points[0], RED.into());
+    draw_point(image, points[1], GREEN.into());
+    draw_point(image, points[2], BLUE.into());
 }
 
 fn draw_line(image: &mut Image, start: UVec2, end: UVec2, color: Color) {
@@ -244,15 +288,7 @@ fn draw_line(image: &mut Image, start: UVec2, end: UVec2, color: Color) {
 }
 
 fn draw_point(image: &mut Image, pos: UVec2, color: Color) {
-    // let half_width = image.size().x / 2;
-    // let half_height = image.size().y / 2;
-    //
-    // let half_size = 32;
-    // for x in half_width - half_size..=half_width + half_size {
-    //     for y in half_height - half_size..=half_height + half_size {
     image.set_color_at(pos.x, pos.y, color).unwrap();
-    //     }
-    // }
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
